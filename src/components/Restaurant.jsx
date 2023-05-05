b974d3fb3f96be70ae3c614f0c47a27cd12e1516
AB-Akram <abdelhakakram79@gmail.com>
2023-05-05 20:47:22 +0200

    you can comment any resto

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
 
 export default function Restaurant (){
     const routeParams = useParams();
     const [data, setData] = useState([])
     const [dataComments, setDataComments] = useState([])
+
+    const [comments, setComments] = useState([])
     const [id, setId] = useState('')
     const [image, setImage] = useState('')
     useEffect(()=>{
@@ -18,8 +24,16 @@ export default function Restaurant (){
     const fetching = (id)=>{
         fetch(`http://127.0.0.1:8000/api/restaurant/comment/${id}/`)
         .then((response) => response.json())
-        .then((data)=> setDataComments(data));
+        .then((data)=> {setDataComments(data);setId(id)});
         }
+
+        const handleSubmit = ()=>{
+            event.preventDefault();
+                axios.post(`http://127.0.0.1:8000/api/restaurant/comments/${id}/`,{
+                comments:comments
+            })
+            setComments('')
+            }
     
     return (
      <>
@@ -42,8 +56,22 @@ export default function Restaurant (){
         {dataComments.comments?.map((data)=>( <Card>
             <Card.Body>{data.comments}</Card.Body>
              </Card>))
-           
         }
+
+    <Form method="POST" onSubmit={handleSubmit}>
+      <FloatingLabel controlId="floatingTextarea2" label="Comments">
+        <Form.Control
+        value={comments}
+          as="textarea"
+          placeholder="Leave a comment here"
+          style={{ height: '100px' }}
+          onChange={(e) => setComments(event.target.value)}
+        />
+      </FloatingLabel>
+      <Button variant="primary" type="submit">
+        Envoyer
+      </Button>
+    </Form>
      </div>
 
      </>   
